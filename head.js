15a3b5dceb36f1e7d14764fc41f93b9d09910bec
Kevin Yang <kevinyang.cyen@gmail.com>
2020-09-14 15:25:55 -0700

    head.js add

---
+++ head.js
@@ -0,0 +1,23 @@
+// FUNCTION IMPLEMENTATION
+const head = function(list) {
+  return list[0];
+}
+
+
+
+
+
+/* TEST CODE
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: '${actual}' === '${expected}'`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Passed: '${actual}' !== '${expected}'`);
+  }
+};
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), undefined);
+*/
