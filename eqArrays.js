8ed99b049de7ece1907bfb1fce3d37fbabf489a5
Kevin Yang <kevinyang.cyen@gmail.com>
2020-09-15 14:15:37 -0700

    added eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+// FUNCTION IMPLEMENTATION
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length === arrayTwo.length) {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+
+
+/* Test Code
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: '${actual}' === '${expected}'`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: '${actual}' !== '${expected}'`);
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+*/
--- head.js
+++ head.js
@@ -4,10 +4,7 @@ const head = function(list) {
 }
 
 
-
-
-
-/* TEST CODE
+//TEST CODE
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: '${actual}' === '${expected}'`);
@@ -20,4 +17,3 @@ const assertEqual = function(actual, expected) {
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([]), undefined);
-*/
