273e6e4ddf285c0bd19d4ae79cc3148c51b21478
Kevin Yang <kevinyang.cyen@gmail.com>
2020-09-15 14:29:11 -0700

    added assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+// FUNCTION IMPLEMENTATION
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: '${actual}' === '${expected}'`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: '${actual}' !== '${expected}'`);
+  }
+};
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length === arrayTwo.length) {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+/* Test Code
+assertArraysEqual([1,2,3], [1,2,4]);
+assertArraysEqual(['1','2','3'], ['1','2',3]);
+assertArraysEqual(['1','2','3'], ['1','2','3']);
+assertArraysEqual([], []);
+*/
