cd3972d1c2a5ae29b281bc3c3baa5c14e14d1ba1
Kevin Yang <kevinyang.cyen@gmail.com>
2020-09-16 14:13:25 -0700

    added letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,43 @@
+// FUNCTION IMPLEMENTATION
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== ' ') {
+      if (!results[sentence[i]]) {
+        results[sentence[i]] = [i];
+      } else {
+        results[sentence[i]].push(i);
+      }
+    }
+  }
+  return results;
+};
+
+/* Test Code
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: '${actual}' === '${expected}'`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: '${actual}' !== '${expected}'`);
+  }
+};
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length === arrayTwo.length) {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        return false;
+      }
+    }
+  }
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  return true;
+};
+
+let lighthouse = "lighthouse in the house";
+let result = letterPositions(lighthouse);
+assertArraysEqual(result.h, [3, 5, 15, 18]);
+assertArraysEqual(result.l, [0]);
+*/
