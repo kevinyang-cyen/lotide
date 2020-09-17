a33a8915cbdda228caad51489c7f1ef90ebe9cff
Kevin Yang <kevinyang.cyen@gmail.com>
2020-09-17 14:40:50 -0700

    added findKeys.js

---
+++ findKey.js
@@ -0,0 +1,41 @@
+// FUNCTION IMPLEMENTATION
+// returns the key of the first element predicate returns truthy for
+const findKey = function(object, callback) {
+  for (let key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return 'No Key with this value'
+}
+
+/* Test Code
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: '${actual}' === '${expected}'`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: '${actual}' !== '${expected}'`);
+  }
+};
+
+test1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+assertEqual(test1, 'noma');
+
+test2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 4) // => "No Key with this value"
+assertEqual(test2, 'No Key with this value');
+*/
