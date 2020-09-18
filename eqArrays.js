af7cdb55db02fed51ad860dbc2025e47f417fd23
Kevin Yang <kevinyang.cyen@gmail.com>
2020-09-18 16:04:46 -0700

    formatting change

--- eqArrays.js
+++ eqArrays.js
@@ -6,11 +6,10 @@ const eqArrays = function(arrayOne, arrayTwo) {
   if (arrayOne.length === arrayTwo.length) {
     for (let i = 0; i < arrayOne.length; i++) {
       if (Array.isArray(arrayOne[i]) && Array.isArray(arrayTwo[i])) {
-        if(!eqArrays(arrayOne[i],arrayTwo[i])) {
+        if (!eqArrays(arrayOne[i],arrayTwo[i])) {
           return false;
         }
-      }
-      else if (arrayOne[i] !== arrayTwo[i]) {
+      } else if (arrayOne[i] !== arrayTwo[i]) {
         return false;
       }
     }
