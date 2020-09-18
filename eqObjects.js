0c4994b3d2e602c04817b796903906e50bca9d49
Kevin Yang <kevinyang.cyen@gmail.com>
2020-09-18 15:36:10 -0700

    changed eqObjects so it checks for both nesting arrays and objects

--- eqObjects.js
+++ eqObjects.js
@@ -1,19 +1,23 @@
 // FUNCTION IMPLEMENTATION
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a false!
-const eqObjects = function(objectOne, objectTwo) {
-  let objectOneKeys = Object.keys(objectOne);
-  let objectTwoKeys = Object.keys(objectTwo);
-  if (objectOneKeys.length !== objectTwoKeys.length) {
+const eqObjects = function(obj1, obj2) {
+  let keys1 = Object.keys(obj1);
+  let keys2 = Object.keys(obj2);
+  if (keys1.length !== keys2.length) {
     return false;
   }
-  for (let i = 0; i < objectOneKeys.length; i++) {
+  for (let i = 0; i < keys1.length; i++) {
     // First check if value is an Array, since comparing two Arrays with !== will always result in a false
-    if (Array.isArray(objectOne[objectOneKeys[i]]) && Array.isArray(objectTwo[objectOneKeys[i]])) {
-      if (!eqArrays(objectOne[objectOneKeys[i]], objectTwo[objectOneKeys[i]])) {
+    if (Array.isArray(obj1[keys1[i]]) && Array.isArray(obj2[keys1[i]])) {
+      if (!eqArrays(obj1[keys1[i]], obj2[keys1[i]])) {
         return false;
       }
-    } else if (objectOne[objectOneKeys[i]] !== objectTwo[objectOneKeys[i]]) {
+    } else if (typeof obj1[keys1[i]] === 'object' && typeof obj2[keys1[i]] === 'object') {
+      if (!eqObjects(obj1[keys1[i]], obj2[keys1[i]])) {
+        return false;
+      }
+    } else if (obj1[keys1[i]] !== obj2[keys1[i]]) {
       return false;
     }
   }
@@ -56,4 +60,8 @@ const cd2 = { c: "1", d: ["2", 3, 4] };
 
 assertEqual(eqObjects(cd, dc), true);
 assertEqual(eqObjects(cd, cd2), false);
+
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true); // => true
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false); // => false
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false); // => false
 */
