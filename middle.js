18abd1d66e5ae1b325c28a46359b24f21c16ef21
Kevin Yang <kevinyang.cyen@gmail.com>
2020-09-21 13:56:04 -0700

    separated the test for middle.js into it's own file

--- middle.js
+++ middle.js
@@ -14,28 +14,4 @@ const middle = function(array) {
   return newArray;
 };
 
-
-
-/* Test Code
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: '${actual}' === '${expected}'`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: '${actual}' !== '${expected}'`);
-  }
-};
-
-const eqArrays = function(arrayOne, arrayTwo) {
-  if (arrayOne.length === arrayTwo.length) {
-    for (let i = 0; i < arrayOne.length; i++) {
-      if (arrayOne[i] !== arrayTwo[i]) {
-        return false;
-      }
-    }
-  }
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  }
-  return true;
-};
-*/
+module.exports = middle;
