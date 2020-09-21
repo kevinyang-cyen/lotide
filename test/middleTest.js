6baeca04eabdad2faf8d4471e0419d6f537db7e8
Kevin Yang <kevinyang.cyen@gmail.com>
2020-09-21 14:38:06 -0700

    refactored middleTest.js and tailTest.js to use mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,22 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([1]),[]);
-assertArraysEqual(middle([1,2]),[]);
-assertArraysEqual(middle([1,2,3]),[2]);
-assertArraysEqual(middle([1,2,3,4]),[2,3]);
-assertArraysEqual(middle([1,2,3,4,5]),[3]);
+describe("#middle", () => {
+
+  it("returns empty array if input array has a length of one", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it("returns empty array if input array has a length of two", () => {
+    assert.deepEqual(middle([1,2]), []);
+  });
+
+  it("returns middle value if input array has an odd number of elements", () => {
+    assert.deepEqual(middle([1,2,3]), [2]);
+  });
+
+  it("returns middle two valuse if input array has an even number of elements", () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]);
+  });
+
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,17 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// Test Case: Check results
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe("#tail", () => {
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  it("returns last two elements from [\"Hello\", \"Lighthouse\", \"Labs\"]", () => {
+    const words = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.deepEqual(words, ["Lighthouse", "Labs"]);
+  });
+
+  it("makes sure original array is not changed", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.strictEqual(words.length, 3); 
+  });
+
+});
